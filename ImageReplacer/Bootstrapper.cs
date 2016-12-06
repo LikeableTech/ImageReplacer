3b4f701a1156deec45e8f67ef1da9f83bb3fcb8e
leadgreenbear <martin.john.mayer@gmail.com>
2016-12-06 00:10:41 +0000

    Add project files.

---
+++ ImageReplacer/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using ImageReplacer.Views;
+using System.Windows;
+
+namespace ImageReplacer
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
